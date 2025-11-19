0b294e271c3c6891d03aef9d72066d19178e8bcd
malina <kstasiuk09@gmail.com>
2025-11-19 23:12:55 +0100

    Create bryly.py

---
+++ bryly.py
@@ -0,0 +1,8 @@
+import math
+import plaskie
+def cude(a:float) -> float:
+    return (a**3)
+def pros(a:float,b:float,c:float) -> float:
+    return 2 * (a*b + b*c + a*c)
+def granbokrownpodkw(a,b) -> float:
+    return (2*(2*(a**2) + plaskie.kwipro(a,b)))
